734fea4bfd4d0c6b46e516acba69c16f46596022
Pavan Kumar K P <pavan.p@s.amity.edu>
2026-08-20 11:16:09 +0530

    Update app.js

--- website/app.js
+++ website/app.js
@@ -204,7 +204,7 @@ async function apiCall(action, data = {}) {
   try {
     const response = await fetch(API_URL, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'text/plain;charset=utf-8' },
       body: JSON.stringify({ action, data })
     });
     
